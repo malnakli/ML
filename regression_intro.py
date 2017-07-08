0742c443752fad0ffe5d6f20740c18b3d34f1378
MoAir <hassoun@outlook.com>
2017-07-08 12:37:42 -0400

    first commit

---
+++ regression_intro.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import quandl, math , datetime
+import numpy as np
+from sklearn import preprocessing, svm
+from sklearn import  model_selection 
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+
+df = quandl.get('WIKI/GOOGL')
+df['HL_PCT'] = ((df['Adj. High'] - df['Adj. Low'])/df['Adj. Low']) * 100
+df['PCT_change'] = ((df['Adj. Open'] - df['Adj. Close'])/df['Adj. Open']) * 100
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+forecast_col ='Adj. Close'
+df.fillna(-9999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR(kernel='poly')
+
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += 86400
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+
+df.to_html('data2.html',classes='table')
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
