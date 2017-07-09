182a39fc73047fa6ee40a21ad362befae5cf7509
MoAir <hassoun@outlook.com>
2017-07-09 06:53:47 -0400

    update formate and gitignore

--- regression_intro.py
+++ regression_intro.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import quandl, math , datetime
+import quandl
+import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, svm
-from sklearn import  model_selection 
+from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -12,14 +14,15 @@ style.use('ggplot')
 
 
 df = quandl.get('WIKI/GOOGL')
-df['HL_PCT'] = ((df['Adj. High'] - df['Adj. Low'])/df['Adj. Low']) * 100
-df['PCT_change'] = ((df['Adj. Open'] - df['Adj. Close'])/df['Adj. Open']) * 100
-df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+df['HL_PCT'] = ((df['Adj. High'] - df['Adj. Low']) / df['Adj. Low']) * 100
+df['PCT_change'] = ((df['Adj. Open'] - df['Adj. Close']) /
+                    df['Adj. Open']) * 100
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
-forecast_col ='Adj. Close'
+forecast_col = 'Adj. Close'
 df.fillna(-9999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
@@ -32,7 +35,8 @@ df.dropna(inplace=True)
 
 y = np.array(df['label'])
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, test_size=0.2)
 
 # clf = LinearRegression(n_jobs=-1)
 # # clf = svm.SVR(kernel='poly')
@@ -41,7 +45,7 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 # with open('linearregression.pickle','wb') as f:
 #     pickle.dump(clf, f)
 
-pickle_in = open('linearregression.pickle','rb')
+pickle_in = open('linearregression.pickle', 'rb')
 clf = pickle.load(pickle_in)
 
 confidence = clf.score(X_test, y_test)
@@ -57,7 +61,7 @@ next_unix = last_unix + one_day
 for i in forecast_set:
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += 86400
-    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
 
 # df.to_html('data2.html',classes='table')
 df['Adj. Close'].plot()
